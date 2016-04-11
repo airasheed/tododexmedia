f2b37d086aef46724036dc893fe7ece9213b5626
wbb <accounts@webuyblack.com>
2016-04-10 20:39:55 -0500

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todoDexMediaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the todoDexMediaApp
+ */
+angular.module('todoDexMediaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
